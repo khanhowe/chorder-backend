d68d2a4e08796d11c319add7aef50d29b3460578
alaska113 <alaska113@gmail.com>
2023-11-29 10:28:48 -0800

    Restart, switching to nest.js

--- src/routes/chordRoutes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { analyzeNotes } from '../controllers/chordController';
-
-const router = Router();
-
-router.post('/analyze-notes', analyzeNotes);
-
-export default router;
